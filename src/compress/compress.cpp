4917c871c42986fde0c5a29a94f56b034366da91
hoangthaiduong <hoangthaiduong@gmail.com>
2019-04-25 15:08:14 -0600

    Compress: parallelism working

--- src/compress/compress.cpp
+++ src/compress/compress.cpp
@@ -1,8 +1,12 @@
+#include <atomic>
+#include <condition_variable>
 #include <cstddef>
 #include <cstdio>
+#include <iostream>
 #include <thread>
 #include <vector>
 #include <stlab/concurrency/default_executor.hpp>
+#include <stlab/concurrency/immediate_executor.hpp>
 #include <stlab/concurrency/future.hpp>
 #include <zfp.h>
 #include "../mg_args.h"
@@ -28,7 +32,7 @@ params ParseParams(int Argc, const char** Argv) {
   params P;
   mg_AbortIf(!GetOptionValue(Argc, Argv, "--input", &P.Input), "Provide --input");
   mg_AbortIf(!GetOptionValue(Argc, Argv, "--output", &P.Output), "Provide --output");
-  mg_AbortIf(!GetOptionValue(Argc, Argv, "--raw_file", &P.Rate), "Provide --bitrate");
+  mg_AbortIf(!GetOptionValue(Argc, Argv, "--rate", &P.Rate), "Provide --rate");
   error Err = ParseMeta(P.Input, &P.Meta);
   mg_AbortIf(ErrorExists(Err), "%s", ToString(Err));
   return P;
@@ -44,13 +48,15 @@ void CopyBlock(f64* Dst, const f64* Src, v3i Dims, v3i Pos) {
   }
 }
 
+int Counter;
+mutex Mutex;
+condition_variable Cond;
+
 void Compress(const params& P) {
   /* mmap the input file */
-  mmap_file MMapIn;
-  auto Err = open_file(P.Input, map_mode::Read, &MMapIn);
+  mmap_file MMapIn; auto Err = open_file(P.Input, map_mode::Read, &MMapIn);
   mg_AbortIf(ErrorExists(Err), "%s", ToString(Err));
-  Err = map_file(&MMapIn);
-  mg_AbortIf(ErrorExists(Err), "%s", ToString(Err));
+  Err = map_file(&MMapIn); mg_AbortIf(ErrorExists(Err), "%s", ToString(Err));
 
   /* mmap the output file */
   mmap_file MMapOut;
@@ -60,31 +66,44 @@ void Compress(const params& P) {
   v3i NumBlocks3 = (P.Meta.Dims + (ZDims - 1)) / ZDims;
   i64 TotalCompressedBytes = Prod<i64>(NumBlocks3) * 8 * P.Rate;
   Err = map_file(&MMapOut, TotalCompressedBytes);
-  zfp_field* Field = zfp_field_3d(NULL, zfp_type_double, 4, 4, 4);
 
-  zfp_stream* Zfp = zfp_stream_open(NULL);
-  zfp_stream_set_rate(Zfp, P.Rate, zfp_field_type(Field), zfp_field_dimensionality(Field), 0);
-  size_t Bytes = zfp_stream_maximum_size(Zfp, Field);
-  zfp_field_free(Field);
   //mg_BeginFor3(V, v3i::Zero(), P.Meta.Dims, ZDims) {
+  Counter = Prod<i64>(NumBlocks3);
   for (i64 BlockId = 0; BlockId < Prod<i64>(NumBlocks3); ++BlockId) { // for each block
-    auto Fut = async(default_executor, [&]() {
-      printf("Block %lld", BlockId);
+    auto Fut = async(immediate_executor, [=]() {
+      //if (BlockId % 100 == 0)
+        //fprintf(stderr, "Block %lld : %f\n", BlockId, S);
       v3i V = IToXyz(BlockId, NumBlocks3) * ZDims;
       f64 Block[64];
       CopyBlock(Block, (f64*)MMapIn.Buf.Data, P.Meta.Dims, V);
+      zfp_field* Field = zfp_field_3d(NULL, zfp_type_double, 4, 4, 4);
+      zfp_stream* Zfp = zfp_stream_open(NULL);
+      zfp_stream_set_rate(Zfp, P.Rate, zfp_field_type(Field), zfp_field_dimensionality(Field), 0);
+      size_t Bytes = zfp_stream_maximum_size(Zfp, Field);
+      zfp_field_free(Field);
       bitstream* Stream = stream_open(MMapOut.Buf.Data + BlockId * 8 * P.Rate, Bytes);
       zfp_stream_set_bit_stream(Zfp, Stream);
       zfp_encode_block_double_3(Zfp, Block);
       zfp_stream_flush(Zfp);
+      {
+        unique_lock<mutex> Lock(Mutex);
+        --Counter;
+      }
+      if (Counter == 0)
+        Cond.notify_all();
     });
+    Fut.detach();
   }
+  unique_lock<mutex> Lock(Mutex);
+  Cond.wait(Lock, []{ return Counter == 0; });
+  //this_thread::sleep_for(chrono::milliseconds(10000));
+  printf("Flusing file...\n");
   Err = flush_file(&MMapOut); mg_AbortIf(ErrorExists(Err), "%s", ToString(Err));
   Err = sync_file(&MMapOut); mg_AbortIf(ErrorExists(Err), "%s", ToString(Err));
   Err = unmap_file(&MMapOut); mg_AbortIf(ErrorExists(Err), "%s", ToString(Err));
   Err = close_file(&MMapOut); mg_AbortIf(ErrorExists(Err), "%s", ToString(Err));
-  Err = unmap_file(&MMapOut); mg_AbortIf(ErrorExists(Err), "%s", ToString(Err));
-  Err = close_file(&MMapOut); mg_AbortIf(ErrorExists(Err), "%s", ToString(Err));
+  Err = unmap_file(&MMapIn); mg_AbortIf(ErrorExists(Err), "%s", ToString(Err));
+  Err = close_file(&MMapIn); mg_AbortIf(ErrorExists(Err), "%s", ToString(Err));
 }
 
 void decompress(const params& P, volume* Vol) {
@@ -93,7 +112,8 @@ void decompress(const params& P, volume* Vol) {
 
 int main(int Argc, const char** Argv) {
   auto P = ParseParams(Argc, Argv);
-
+  Compress(P);
+  std::cout << Counter << "\n";
   //auto x = async(default_executor, [] { return 42; });
 
   //auto y = x.then([](int x) { printf("Result %d \n", x); });
