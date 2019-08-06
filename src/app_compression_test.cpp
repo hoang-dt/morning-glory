ce2ff2f51fdf1094ad9da5af335d3ad30763c8de
duong <hoangthaiduong@gmail.com>
2019-08-06 13:12:39 -0600

    zfp 2d now works

--- src/app_compression_test.cpp
+++ src/app_compression_test.cpp
@@ -199,17 +199,7 @@ void TestJp2k2D() {
   memset(&DParams, 0, sizeof(opj_dparameters_t));
   DParams.decod_format = 0; // J2K_CFMT;
   DParams.cod_format = 18; // RAWL_DFMT 18 /* LSB / Little Endian */
-  // force precision
-  //DParams.prec = image->comps[compno].prec;
-  //DParams.precision = 
-  //DParams.nb_precision = 1;
-  //parameters->core.cp_reduce
-  //parameters->core.cp_layer
-  // decode roi
-  //parameters->DA_x0, & parameters->DA_y0,
-  //parameters->DA_x1, & parameters->DA_y1
   // decode tile
-  //parameters->nb_tile_to_decode
   u32 CompsIndices = 0;
   opj_set_default_decoder_parameters(&DParams);
   auto DStream = opj_stream_create_default_file_stream("jp2k-out.raw", 1);
@@ -237,36 +227,9 @@ void TestJp2k2D() {
   opj_image_destroy(DImage);
   opj_stream_destroy(DStream);
   opj_destroy_codec(Decompressor);
-  // free
-  //if (parameters->precision) {
-  //  free(parameters->precision);
-  //  parameters->precision = NULL;
-  //}
-
-  
-  //auto CompressedBuf = Stream->buffer;
-  //auto Length = cio_tell(Stream);
-  ////cio_seek(Stream, 0);
-  //opj_dinfo_t* Decompressor = opj_create_decompress(CODEC_J3D);
-  //opj_dparameters_t DParams;
-  //opj_set_default_decoder_parameters(&DParams); // TODO
-  //DParams.decod_format = J3D_CFMT;
-  //opj_setup_decoder(Decompressor, &DParams);
-  //opj_cio_t* DStream = opj_cio_open((opj_common_ptr)Decompressor, CompressedBuf, Length);
-  //opj_volume_t* OutVol = opj_decode(Decompressor, DStream);
-  ////FILE* Fp = fopen("decompressed.raw", "wb");
-  ////fwrite(OutVol->comps[0].data, Prod(Dims3) * sizeof(int), 1, Fp);
-  ////fclose(Fp);
-  ////OutVol->comps[0]->data;
-  //auto Psnr = PSNR(QVol, volume(OutVol->comps[0].data, Dims(QVol)));
-  //printf("psnr = %f\n", Psnr);
-  //DeallocBuf(&QVol.Buffer);
-  //opj_cio_close(Stream);
-  //opj_cio_close(DStream);
-  //opj_volume_destroy(OutVol);
-  //opj_destroy_decompress(Decompressor);
 }
 
+// TODO: use RealDims3 instead of TileDims3
 void TestZfp2() {
   /* read the data from disk */
   v3i TileDims3(CBlock_);
@@ -405,6 +368,153 @@ void TestZfp2() {
   printf("zfp size %lld, time: %f %f\n", TotalCompressedSize, Seconds(TotalCompressionTime), Seconds(TotalDecompressionTime));
 }
 
+void TestZfp2D() {
+  /* read the data from disk */
+  v3i TileDims3(CBlock_, CBlock_, 1);
+  volume Vol;
+  ReadVolume(DataFile_, InputDims_, InputType_, &Vol);
+  volume QVol(Dims(Vol), dtype::int32);
+  v3i Dims3 = Dims(QVol);
+  Quantize(Prec_, Vol, &QVol);
+  //Clone(Vol, &QVol);
+  ForwardCdf53Old(&QVol, NLevels_);
+  array<extent> Sbands; BuildSubbands(Dims(QVol), NLevels_, &Sbands);
+  buffer BufQ; AllocBuf(&BufQ, SizeOf(QVol.Type) * Prod(TileDims3));
+  volume TileVolQ(BufQ, TileDims3, QVol.Type); // quantized tile data
+  buffer BufN; AllocBuf(&BufN, SizeOf(QVol.Type) * Prod(TileDims3));
+  volume TileVolN(BufN, TileDims3, IntType(UnsignedType(TileVolQ.Type))); // negabinary
+  buffer BufO; AllocBuf(&BufO, SizeOf(TileVolN.Type) * Prod(TileDims3));
+  volume TileVolO(BufO, TileDims3, TileVolN.Type); // decompression output
+  buffer BsBuf; AllocBuf(&BsBuf, Vol.Buffer.Bytes);
+  bitstream Bs; InitWrite(&Bs, BsBuf);
+  timer Timer;
+  i64 TotalCompressionTime = 0;
+  i64 TotalDecompressionTime = 0;
+  i64 TotalCompressedSize = 0;
+  /* -------- encode the data --------- */
+  StartTimer(&Timer);
+  InitWrite(&Bs, Bs.Stream);
+  v3i BlockDims3(4, 4, 1);
+  v3i NBlocks3 = (TileDims3 + BlockDims3 - 1) / BlockDims3;
+  array<i8> Ns; Init(&Ns, Prod(NBlocks3), i8(0));
+  //FILE* Fp = fopen("encode.raw", "wb");
+  FILE* Fp2 = fopen("encode.txt", "w");
+  for (int Sb = 0; Sb < Size(Sbands); ++Sb) { // through subbands
+    v3i SbFrom3 = From(Sbands[Sb]);
+    v3i SbDims3 = Dims(Sbands[Sb]);
+    v3i NTiles3 = (SbDims3 + TileDims3 - 1) / TileDims3;
+    v3i T; // tile counter
+    mg_BeginFor3(T, v3i::Zero, NTiles3, v3i::One) { // through tiles
+      Fill(Begin<u32>(TileVolO), End<u32>(TileVolO), 0);
+      Fill(Begin<u32>(TileVolN), End<u32>(TileVolN), 0);
+      Fill(Begin<i8>(Ns), End<i8>(Ns), 0);
+      v3i TileFrom3 = SbFrom3 + TileDims3 * T;
+      v3i RealDims3 = Min(SbFrom3 + SbDims3 - TileFrom3, TileDims3);
+      v3i RealNBlocks3 = (RealDims3 + BlockDims3 - 1) / BlockDims3;
+      v3i P3;
+      // TODO: there is a bug if the tile is less than 32^3
+      //mg_Assert(RealDims3 == TileDims3);
+      mg_BeginFor3(P3, v3i::Zero, RealNBlocks3, v3i::One) { // through blocks
+        int S = Row(NBlocks3, P3) * Prod(BlockDims3);
+        v3i D3 = P3 * BlockDims3 + TileFrom3;
+        v3i B3;
+        mg_BeginFor3(B3, v3i::Zero, BlockDims3, v3i::One) {
+          mg_Assert(B3.Z == 0);
+          TileVolQ.At<i32>(S + Row(BlockDims3, B3)) = QVol.At<i32>(D3 + B3);
+        } mg_EndFor3
+        //fwrite((i32*)TileVolQ.Buffer.Data + S, Prod(BlockDims3) * sizeof(i32), 1, Fp);
+        ForwardZfp2D<i32, 4>((i32*)TileVolQ.Buffer.Data + S);
+        for (int I = 0; I < 4 * 4; ++I) {
+          i32 Val = *((i32*)TileVolQ.Buffer.Data + S + I);
+          if (!(Val >= -(1 << 18) && Val < (1 << 18)))
+            printf("!!!!!!! overflow !!!!!!!!\n");
+        }
+        ForwardShuffle2D<i32, u32, 4>((i32*)TileVolQ.Buffer.Data + S, (u32*)TileVolN.Buffer.Data + S);
+        for (int I = 0; I < 16; ++I)
+          fprintf(Fp2, "%u\n", *((u32*)TileVolN.Buffer.Data + S + I));
+      } mg_EndFor3 // end sample loop
+      int NBitplanes = Prec_ + 1 + 2; //Prec + 1;
+      int Bp = NBitplanes - 1;
+      while (Bp >= 0) { // through bit planes
+        for (int B = 0; B < Prod(NBlocks3); ++B) {
+          int BX = B % NBlocks3.X, BY = B / NBlocks3.X;
+          if (BX >= RealNBlocks3.X || BY >= RealNBlocks3.Y)
+            continue;
+          u32* Beg = (u32*)TileVolN.Buffer.Data + B * Prod(BlockDims3);
+          Encode<u32, 2, 4>(Beg, Bp, 2e9, Ns[B], &Bs);
+        }
+        --Bp;
+      } // end bit plane loop
+    } mg_EndFor3 // end tile loop
+  } // end subband loop
+  TotalCompressionTime += ElapsedTime(&Timer);
+  Flush(&Bs);
+  TotalCompressedSize += Size(Bs);
+  //return;
+  //fclose(Fp);
+  fclose(Fp2);
+
+  /* ---------- decode ---------- */
+  //Fp = fopen("decode.raw", "wb");
+  Fp2 = fopen("decode.txt", "w");
+  volume QVolBackup; Clone(QVol, &QVolBackup);
+  InverseCdf53Old(&QVolBackup, NLevels_);
+  Fill(Begin<i32>(QVol), End<i32>(QVol), 0);
+  InitRead(&Bs, Bs.Stream);
+  StartTimer(&Timer);
+  array<i8> NOs; Init(&NOs, Prod(NBlocks3), i8(0));
+  for (int Sb = 0; Sb < Size(Sbands); ++Sb) { // through subbands
+    v3i SbFrom3 = From(Sbands[Sb]);
+    v3i SbDims3 = Dims(Sbands[Sb]);
+    v3i NTiles3 = (SbDims3 + TileDims3 - 1) / TileDims3;
+    v3i T; // tile counter
+    mg_BeginFor3(T, v3i::Zero, NTiles3, v3i::One) { // through tiles
+      Fill(Begin<u32>(TileVolO), End<u32>(TileVolO), 0);
+      Fill(Begin<i8>(NOs), End<i8>(NOs), 0);
+      v3i TileFrom3 = SbFrom3 + TileDims3 * T;
+      v3i RealDims3 = Min(SbFrom3 + SbDims3 - TileFrom3, TileDims3);
+      v3i RealNBlocks3 = (RealDims3 + BlockDims3 - 1) / BlockDims3;
+      /* decompress here */
+      int NBitplanes = Prec_ + 1 + 2;
+      int Bp = NBitplanes - 1;
+      while (Bp >= 0) {
+        for (int B = 0; B < Prod(NBlocks3); ++B) {
+          int BX = B % NBlocks3.X, BY = B / NBlocks3.X;
+          if (BX >= RealNBlocks3.X || BY >= RealNBlocks3.Y)
+            continue;
+          u32* Beg = (u32*)TileVolO.Buffer.Data + B * Prod(BlockDims3);
+          Decode<u32, 2, 4>(Beg, Bp, 2e9, NOs[B], &Bs);
+        }
+        --Bp;
+      }
+      v3i P3;
+      mg_BeginFor3(P3, v3i::Zero, RealNBlocks3, v3i::One) { // through blocks
+        int S = Row(NBlocks3, P3) * Prod(BlockDims3);
+        v3i D3 = P3 * BlockDims3 + TileFrom3;
+        mg_Assert(D3.Z == 0);
+        for (int I = 0; I < 16; ++I)
+          fprintf(Fp2, "%u\n", *((u32*)TileVolO.Buffer.Data + S + I));
+        InverseShuffle2D<i32, u32, 4>((u32*)TileVolO.Buffer.Data + S, (i32*)TileVolQ.Buffer.Data + S);
+        InverseZfp2D<i32, 4>((i32*)TileVolQ.Buffer.Data + S);
+        v3i B3;
+        mg_BeginFor3(B3, v3i::Zero, BlockDims3, v3i::One) {
+          QVol.At<i32>(D3 + B3) = TileVolQ.At<i32>(S + Row(BlockDims3, B3));
+        } mg_EndFor3
+      } mg_EndFor3
+    } mg_EndFor3
+  }
+  auto DecompressedSize = Size(Bs);
+  printf("decompressed size = %lld\n", DecompressedSize);
+  TotalDecompressionTime += ElapsedTime(&Timer);
+  InverseCdf53Old(&QVol, NLevels_);
+  //WriteBuffer("out.raw", QVol.Buffer);
+  auto Psnr = PSNR(QVolBackup, QVol);
+  //fclose(Fp);
+  fclose(Fp2);
+  printf("psnr = %f\n", Psnr);
+  /* write the bit stream */
+  printf("zfp size %lld, time: %f %f\n", TotalCompressedSize, Seconds(TotalCompressionTime), Seconds(TotalDecompressionTime));
+}
 void TestZfp(volume& Vol, int NLevels, metadata Meta, const v3i& TileDims3, f64 Tolerance) {
   /* perform wavelet transform and allocate necessary buffers */
   ForwardCdf53Old(&Vol, NLevels);
@@ -681,8 +791,8 @@ int main(int Argc, const char** Argv) {
   //  }
   //}
 
-  TestJp2k2D();
-  //TestZfp2();
+  //TestJp2k2D();
+  TestZfp2D();
   /* Read data */
   //cstr InputFile, OutputFile;
   //mg_AbortIf(!OptVal(Argc, Argv, "--input", &InputFile), "Provide --input");
